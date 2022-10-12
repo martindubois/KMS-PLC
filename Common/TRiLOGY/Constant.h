132e6960223dddbda303330bab2b91ef1dc30e7b
Martin Dubois <mdubois@kms-quebec.com>
2022-10-12 09:57:17 -0400

    Adding license link

--- Common/TRiLOGY/Constant.h
+++ Common/TRiLOGY/Constant.h
@@ -1,6 +1,7 @@
 
 // Author    KMS - Martin Dubois, P. Eng.
 // Copyright (C) 2022 KMS
+// License   http://www.apache.org/licenses/LICENSE-2.0
 // Product   KMS-PLC
 // File      Common/TRiLOGY/Constant.h
 
